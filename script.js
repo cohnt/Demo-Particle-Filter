74bce8bdbb2e66d601997dfdeea205dd390eb967
Thomas Cohn <cohnt@umich.edu>
2018-07-30 11:42:22 -0400

    CANVAS: Add function to draw a path (to be used with the mouse and the guess).

--- script.js
+++ script.js
@@ -5,7 +5,11 @@
 var canvasSize = {width: 1000, height: 600}; //Given in pixels
 var pillarLocation = [500, 300];
 var pillarRadius = 10;
+var pillarStrokeStyle = "black";
 var pillarFillStyle = "#999999";
+var mousePathColor = "black";
+var guessPathColor = "red";
+var pathMarkerSize = 4; //It's a square
 
 ///////////////////////////////////////////
 /// GLOBAL VARIABLES
@@ -15,6 +19,8 @@ var canvas;            //The html object for the canvas
 var ctx;               //2D drawing context for the canvas
 var inCanvas = false;  //Whether or not the mouse is in the canvas
 var mousePos = [0, 0]; //[x, y] location of the mouse in the canvas
+var mousePath = [];    //List of [x, y] locations the mouse was at each sample
+var guessPath = [];    //The particle filter's best guess of the mouse's path
 
 ///////////////////////////////////////////
 /// CLASSES
@@ -56,6 +62,7 @@ function mouseMoveCanvas(event) {
 }
 
 function drawPillar() {
+	ctx.strokeStyle = pillarStrokeStyle;
 	ctx.beginPath();
 	ctx.moveTo(pillarLocation[0] + pillarRadius, pillarLocation[1]);
 	ctx.arc(pillarLocation[0], pillarLocation[1], pillarRadius, 0, 2*Math.PI, true);
@@ -64,6 +71,26 @@ function drawPillar() {
 
 	ctx.fillStyle = pillarFillStyle;
 	ctx.fill();
+}
+function drawPath(path, color) {
+	ctx.strokeStyle = color;
+	ctx.fillStyle = color;
+
+	//Draw edges
+	if(path.length > 1) {
+		ctx.beginPath();
+		ctx.moveTo(path[0][0], path[0][1]);
+		for(var i=1; i<path.length; ++i) {
+			ctx.lineTo(path[i][0], path[i][1]);
+		}
+		ctx.stroke();
+	}
+
+	//Draw markers
+	var foo = pathMarkerSize;
+	for(var i=0; i<path.length; ++i) {
+		ctx.fillRect(path[i][0] - (foo/2), path[i][1] - (foo/2), foo, foo);
+	}
 }
 
 ///////////////////////////////////////////
